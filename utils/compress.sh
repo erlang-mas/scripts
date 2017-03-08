dec86b99f00b9201ebc3f764e765f0d7e8d9a0cf
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-03-08 03:36:34 +0100

    Add results compression utility script

---
+++ utils/compress.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+#SBATCH -J emas-compress
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=03:00:00
+#SBATCH -A plglogin
+#SBATCH -p plgrid
+
+results_path=$1
+
+cd $SCRATCH/erlang-mas
+
+compressed_path=$results_path
+compressed_path+=".tar.gz"
+
+srun tar -zcvf $compressed_path $results_path
