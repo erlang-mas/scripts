a054a5d215b9701723c5731ab3fb850e3db9ff97
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-03-11 19:00:42 +0100

    Decrease default node migration probability

--- run/slurm/run_node.sh
+++ run/slurm/run_node.sh
@@ -17,7 +17,7 @@ export NIF_DIR=$emas_root/priv
 $emas_root/emas --population-count=24 \
                 --population-size=100 \
                 --migration-probability=0.001 \
-                --node-migration-probability=0.001 \
+                --node-migration-probability=0.0001 \
                 --time=90000 \
                 --output=$experiment_dir
 
