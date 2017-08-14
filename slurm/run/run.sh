70f446dde9291fdfe7b6dd1061f1dff8e42883f6
Bartosz Zurkowski <zurkowski.bartosz@gmail.com>
2017-08-14 20:06:39 +0200

    Remove repetitions from main run script

--- slurm/run/run.sh
+++ slurm/run/run.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-migration_probability=$4
-node_migration_probability=$5
-topology=$6
-nodes_topology=$7
-simulation_name=$8
-
-nodes_min=$1
-nodes_max=$2
-nodes_step=$3
+nodes_count=$1
+migration_probability=$2
+node_migration_probability=$3
+topology=$4
+nodes_topology=$5
+simulation_name=$5
 
 # simulation_name=`date +%s`
 # simulation_name+=_$nodes_min
@@ -21,17 +18,13 @@ user=`id -u -n`
 
 scripts_dir=$HOME/erlang-mas/scripts/slurm/run
 
-for num_nodes in $(seq $nodes_min $nodes_step $nodes_max); do
-  printf $num_nodes
-
-  sbatch --partition=$partition \
-         --account=$account \
-         --nodes=$num_nodes \
-         --ntasks=$num_nodes \
-         --cpus-per-task=24 \
-         --time=$time \
-         --job-name=emas-$num_nodes \
-         --mail-type=ALL \
-         --mail-user=$user \
-         $scripts_dir/run_nodes.sh $simulation_name $num_nodes $migration_probability $node_migration_probability $topology $nodes_topology
-done
+sbatch --partition=$partition \
+       --account=$account \
+       --nodes=$nodes_count \
+       --ntasks=$nodes_count \
+       --cpus-per-task=24 \
+       --time=$time \
+       --job-name=emas-$nodes_count \
+       --mail-type=ALL \
+       --mail-user=$user \
+       $scripts_dir/run_nodes.sh $simulation_name $nodes_count $migration_probability $node_migration_probability $topology $nodes_topology
